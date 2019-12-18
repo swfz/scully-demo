6b1f23884bd5f6bfd6d601a8732ccf683ecfda53
swfz <sawafuji.09@gmail.com>
2019-12-19 07:47:40 +0900

    users routingの追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'users', loadChildren: () => import('./user/user.module').then(mod => mod.UserModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
