ef118210476d97d0a07655b22a813dee3bf656cf
swfz <sawafuji.09@gmail.com>
2019-12-19 07:48:16 +0900

    jsonplaceholderのAPIからHTMLを生成するように設定

--- scully.config.js
+++ scully.config.js
@@ -1,5 +1,12 @@
 exports.config = {
   projectRoot: "./src/app",
   routes: {
+    "/users/:userId": {
+      type: "json",
+      userId: {
+        url: 'https://jsonplaceholder.typicode.com/users',
+        property: 'id'
+      }
+    }
   }
 };
